38567d264641b65305f980bc0fe4f12f6c08add9
Rafael Cronemberger <rafael@berger.com.br>
2023-07-15 16:50:12 -0300

    Packet updates with build yaml

---
+++ src/Berger.Extensions.Cryptography/Helpers/HashHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace Berger.Extensions.Cryptography
+{
+    public static class HashHelper
+    {
+        public static bool Validate(this string password, string salt, string hash) => CreateSalt(password, salt) == hash;
+        public static string CreateSalt(this string password, string salt)
+        {
+            var bytes = KeyDerivation.Pbkdf2
+            (
+                password: password,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8,
+                prf: KeyDerivationPrf.HMACSHA512,
+                salt: salt.ToByteArray()
+            );
+
+            return Convert.ToBase64String(bytes);
+        }
+        public static string CreateHash()
+        {
+            byte[] random = new byte[128 / 8];
+
+            using var generator = RandomNumberGenerator.Create();
+
+            generator.GetBytes(random);
+
+            return Convert.ToBase64String(random);
+        }
+        public static byte[] ToByteArray(this string value)
+        {
+            return Encoding.ASCII.GetBytes(value);
+        }
+    }
+}
